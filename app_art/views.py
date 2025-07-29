83d53abb01355448cce70b1b0adc95acdeec3f10
VinczeDan <vincze.daniel.22f@szlgbp.hu>
2025-07-29 20:35:37 +0200

    p3

--- app_art/views.py
+++ app_art/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
 def index(request):
     return render(request, 'app_art/index.html')
+
+
+
+def health_check(request):
+    return JsonResponse({"status": "ok"})
--- revfalvi_art/urls.py
+++ revfalvi_art/urls.py
@@ -5,8 +5,10 @@ from django.urls import include
 from app_art import views
 from django.conf import settings
 from django.conf.urls.static import static
+from app_art.views import index, health_check
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
+    path('health/', health_check, name='health-check'),
 ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
